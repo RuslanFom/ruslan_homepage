308d3cb992615880e5e9a74a62196919c56a7fe0
Ruslan Fomin <ruslanfom@gmail.com>
2021-11-05 19:58:54 +0300

    fix styles of examples and works

--- pages/examples.js
+++ pages/examples.js
@@ -1,35 +1,37 @@
-import {AspectRatio, Container, Heading, SimpleGrid} from '@chakra-ui/react'
+import { AspectRatio, Container, Heading, SimpleGrid } from '@chakra-ui/react'
 import Layout from '../components/layouts/article'
 import Section from '../components/section'
-import {WorkImage} from "../components/work"
+import { WorkImage } from '../components/work'
 
 const Code = () => (
-    <Layout title='examples'>
-        <Container>
-            <Heading as="h4" fontSize={20} mb={4} mt={4}>
-                Examples of the code
-            </Heading>
-            <Section delay={0.1}>
-                <SimpleGrid columns={[1, 2]} gap={3}>
-                    <AspectRatio maxW="400px" maxH="150px" ratio={16/9}>
-                        <WorkImage src="/images/works/examples/api.png" alt="api" objectFit="cover" />
-                    </AspectRatio>
-                    <AspectRatio maxW="400px" maxH="150px" ratio={16/9}>
-                        <WorkImage src="/images/works/examples/auth-reducer.png" alt="auth-reducer" objectFit="cover" />
-                    </AspectRatio>
-                    <AspectRatio maxW="400px" maxH="150px" ratio={16/9}>
-                        <WorkImage src="/images/works/examples/app.test.png" alt="app.test" objectFit="cover" />
-                    </AspectRatio>
-                    <AspectRatio maxW="400px" maxH="150px" ratio={16/9}>
-                        <WorkImage src="/images/works/examples/App.png" alt="App" objectFit="cover" />
-                    </AspectRatio>
-                    {/*<Section>
+  <Layout title='examples'>
+    <Container>
+      <Section>
+        <Heading as="h3" variant="section-title" p={3}>
+            Code examples
+        </Heading>
+        <Section delay={0.1}>
+          <SimpleGrid columns={[1, 2]} gap={3}>
+            <AspectRatio maxW='400px' maxH='150px' ratio={16 / 9}>
+              <WorkImage src='/images/works/examples/api.png' alt='api' objectFit='cover' />
+            </AspectRatio>
+            <AspectRatio maxW='400px' maxH='150px' ratio={16 / 9}>
+              <WorkImage src='/images/works/examples/auth-reducer.png' alt='auth-reducer' objectFit='cover' />
+            </AspectRatio>
+            <AspectRatio maxW='400px' maxH='150px' ratio={16 / 9}>
+              <WorkImage src='/images/works/examples/app.test.png' alt='app.test' objectFit='cover' />
+            </AspectRatio>
+            <AspectRatio maxW='400px' maxH='150px' ratio={16 / 9}>
+              <WorkImage src='/images/works/examples/App.png' alt='App' objectFit='cover' />
+            </AspectRatio>
+            {/*<Section>
                         <WorkImage src="/images/works/garbage.jpg"/>
                     </Section>*/}
-                </SimpleGrid>
-            </Section>
-        </Container>
-    </Layout>
+          </SimpleGrid>
+        </Section>
+      </Section>
+    </Container>
+  </Layout>
 )
 
 export default Code
--- pages/works.js
+++ pages/works.js
@@ -1,51 +1,53 @@
-import {Container, Heading, SimpleGrid} from '@chakra-ui/react'
+import { Container, Heading, SimpleGrid } from '@chakra-ui/react'
 import Section from '../components/section'
-import {WorkGridItem} from '../components/grid-item'
+import { WorkGridItem } from '../components/grid-item'
 import thumbSN from '../public/images/works/sn.png'
 import thumbTA from '../public/images/works/testAppLogin.png'
 import thumbQR from '../public/images/works/examples/QRepublik1eng.png'
-import Layout from "../components/layouts/article"
+import Layout from '../components/layouts/article'
 
 const Works = () => {
-    return (
-        <Layout>
-        <Container>
-            <Heading as="h3" fontSize={20} mb={4}>
-                Works
-            </Heading>
+  return (
+    <Layout>
+      <Container>
+        <Section>
+          <Heading as="h3" variant="section-title" p={3}>
+            Works
+          </Heading>
 
-            <SimpleGrid columns={[1, 1, 2]} gap={6}>
-                <Section>
-                    <WorkGridItem
-                      id="QRepublik"
-                      title="QRepublik"
-                      thumbnail={thumbQR}
-                    >
-                        My last job, landing-page for QRepublik company
-                    </WorkGridItem>
-                </Section>
-                <Section>
-                    <WorkGridItem
-                        id="Social-media-app"
-                        title="Social-media-app"
-                        thumbnail={thumbSN}
-                    >
-                        My pet-project, in which I implement and consolidate all the material I have learned
-                    </WorkGridItem>
-                </Section>
-                <Section>
-                    <WorkGridItem
-                        id="ChartsApp"
-                        title="ChartsApp"
-                        thumbnail={thumbTA}
-                    >
-                        My statement of work from the employer
-                    </WorkGridItem>
-                </Section>
-            </SimpleGrid>
-        </Container>
-        </Layout>
-    )
+          <SimpleGrid columns={[1, 1, 2]} gap={6}>
+            <Section>
+              <WorkGridItem
+                id='QRepublik'
+                title='QRepublik'
+                thumbnail={thumbQR}
+              >
+                My last job, landing-page for QRepublik company
+              </WorkGridItem>
+            </Section>
+            <Section>
+              <WorkGridItem
+                id='Social-media-app'
+                title='Social-media-app'
+                thumbnail={thumbSN}
+              >
+                My pet-project, in which I implement and consolidate all the material I have learned
+              </WorkGridItem>
+            </Section>
+            <Section>
+              <WorkGridItem
+                id='ChartsApp'
+                title='ChartsApp'
+                thumbnail={thumbTA}
+              >
+                My statement of work from the employer
+              </WorkGridItem>
+            </Section>
+          </SimpleGrid>
+        </Section>
+      </Container>
+    </Layout>
+  )
 }
 
 export default Works
